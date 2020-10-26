ef1cd304090ba1638f01080af1d213b91446f547
Yajnavalkya Bandyopadhyay <yajnab@gmail.com>
2020-10-26 14:24:28 +0530

    yajnabpf.js : Image Loadup function added

--- yajnabpf.js
+++ yajnabpf.js
@@ -1,3 +1,7 @@
 $(function(){
 		var giscanvas = document.getElementById('giswork');
-}
+		var ctx = giscanvas.getContext("2d");
+		const img = new Image();
+		img.src = "./testimg/india.png";
+		img.onload = () => {  ctx.drawImage(img, 0, 0);}
+});
