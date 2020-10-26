0b3c7fe12d59dfc134a641da4df89818be5480bf
Yajnavalkya Bandyopadhyay <yajnab@gmail.com>
2020-10-26 16:24:41 +0530

    yajnabpf.js : Fix canvas width and height

--- yajnabpf.js
+++ yajnabpf.js
@@ -1,7 +1,11 @@
 $(function(){
 		var giscanvas = document.getElementById('giswork');
-		var ctx = giscanvas.getContext("2d");
 		const img = new Image();
 		img.src = "./testimg/india.png";
-		img.onload = () => {  ctx.drawImage(img, 0, 0);}
+		giscanvas.width  = (img.width);
+		giscanvas.height = (img.height);
+		var ctx = giscanvas.getContext("2d");
+		img.onload = () => {
+			ctx.drawImage(img, 0, 0, img.width*0.5, img.height*0.5);
+		}
 });
