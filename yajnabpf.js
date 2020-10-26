1d3963cbe34a7dfc63c12a7966eebc1487d08206
Yajnavalkya Bandyopadhyay <yajnab@gmail.com>
2020-10-26 19:34:11 +0530

    [BUGFIX] yajnabpf.js : Load Canvas after the image has loaded
    Earlier the canvas was loading first which was causing the image loading only
    after the page was refreshed.
    This commit solves the bug

--- yajnabpf.js
+++ yajnabpf.js
@@ -1,11 +1,11 @@
 $(function(){
-		var giscanvas = document.getElementById('giswork');
 		const img = new Image();
 		img.src = "./testimg/india.png";
-		giscanvas.width  = (img.width);
-		giscanvas.height = (img.height);
-		var ctx = giscanvas.getContext("2d");
-		img.onload = () => {
+		img.onload = function() {
+			var giscanvas = document.getElementById('giswork');
+			giscanvas.width  = (img.width);
+			giscanvas.height = (img.height);
+			var ctx = giscanvas.getContext("2d");
 			ctx.drawImage(img, 0, 0, img.width*0.5, img.height*0.5);
-		}
+		};
 });
